b4523bf63cac9134bf6c10d35249d3ebc37587ba
abbosxon <abbosxon.oripov@yandex.ru>
2022-07-25 16:42:21 +0500

    add mult and sum function

---
+++ index.js
@@ -0,0 +1,50 @@
+'classwork'
+// // let datas = [
+// //     { income: 500, expanse: 200 },
+// //     { income: 500, expanse: 200 },
+// //     { income: 500, expanse: 200 },
+// //     { income: 500, expanse: 200 },
+// // ]
+
+// // let finance = (data, str) =>
+// //     data.reduce((acc, el) => el[str] ? acc += el[str] : acc += (el['income'] - el['expance']), 0)
+
+
+// // console.log(finance(datas, 'expanse'));
+
+// function moveZere(arr) {
+//     for (let i = arr.length; i > 0; i--) {
+//         if (arr[i] == 0) {
+//             arr.splice(i, 1)
+//             arr.push(0)
+//         }
+//     }
+//     return arr
+// }
+
+// let array = [1, 2, 3, 4, -1, 0, 5, 0, 10]
+// let number = 5
+// console.log(moveZere(array));
+
+
+'birinchi'
+let mult = (a, b) => {
+    if (b == 0) return 0
+    return a + mult(a, b - 1)
+}
+
+console.log(mult(3, 2));
+
+
+'ikkinchi'
+let number = 35678
+
+function sum(num, col = 0) {
+    col++
+    if (num < 1) return 0
+    return (parseInt(num.pop()) + sum(num))
+}
+number = number.toString().split('')
+
+
+console.log(sum(number));
